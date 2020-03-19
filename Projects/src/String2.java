442e1a36c8dc4ac686a02b2fdccbe2acb7db890f
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2020-03-19 11:54:38 -0400

    Update String2.java

--- Projects/src/String2.java
+++ Projects/src/String2.java
@@ -41,6 +41,61 @@ public class String2 {
 		}
 		return false;
 	}
+	
+	public int countCode(String str) {
+		int count = 0;
+		for(int i=0; i<str.length()-3; i++){
+		  if(str.substring(i,i+2).equals("co") && str.substring(i+3,i+4).equals("e")){
+		      count++;
+		  }
+		}
+		return count;
+	}
+	
+	public boolean endOther(String a, String b) {
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+		if(a.endsWith(b) || b.endsWith(a)){
+		    return true;
+		}
+		return false;
+	}
+	
+	public boolean xyzThere(String str) {
+		if(str.startsWith("xyz")){
+			return true;
+		}
+		for(int i=0; i<str.length()-3; i++){
+		  if(str.charAt(i)!='.'){
+		    if(str.substring(i+1,i+4).equals("xyz")){
+		        return true;
+		    }
+		  }
+		}
+		return false;
+	}
+	
+	public boolean bobThere(String str) {
+		for(int i=0; i<str.length()-2; i++){
+			if(str.charAt(i)=='b' && str.charAt(i+2)=='b'){
+		      return true;
+		      }
+		}
+		return false;
+	}
+	
+	public boolean xyBalance(String str) {
+		int len = str.length() - 1;
+		char ch;
+		for(int i = len; i >= 0; i--) {
+			ch = str.charAt(i);
+			if(ch == 'x')
+				return false;
+			else if(ch == 'y')
+				return true;
+		}
+		return true;  
+	}
 
 
 	public static void main(String[] args) {
