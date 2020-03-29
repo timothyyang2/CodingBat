9e31f760d94be19899f078b003583c2c4760d362
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2020-03-28 22:22:36 -0400

    Updates

--- Projects/src/Array2.java
+++ Projects/src/Array2.java
@@ -38,8 +38,52 @@ public class Array2 {
 			}
 			return (sum-max-min) / (nums.length - 2);
 		}
-
 	
+	public int sum13(int[] nums) {
+		int count = 0;
+		for(int i=0; i<nums.length; i++){
+			if(nums[i]!=13){
+		    count = count + nums[i];
+		    }else{
+		    i++;
+		    }
+		  }
+		  return count;
+		}
+	
+	public int sum67
+
+	public boolean has22(int[] nums) {
+		  for(int i=0; i<nums.length-1; i++){
+		    if(nums[i]==2 && nums[i+1]==2){
+		      return true;
+		    }
+		  }
+		  return false;
+		}
+
+	public boolean lucky13(int[] nums) {
+		  for(int i=0; i<nums.length; i++){
+		    if(nums[i]==1 || nums[i]==3){
+		      return false;
+		    }
+		  }
+		  return true;
+		}
+
+	public boolean sum28(int[] nums) {
+		  int count = 0;
+		  for(int i=0; i<nums.length; i++){
+		    if(nums[i]==2){
+		      count = count + nums[i];
+		    }
+		  }
+		  if(count==8){
+		    return true;
+		  }
+		  return false;
+		}
+
 	public static void main(String[] args) {
 		
 	}
