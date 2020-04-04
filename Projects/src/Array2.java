1403efe2c757022fe5430e18edfebde41312e0c9
timothyyang2 <33989303+timothyyang2@users.noreply.github.com>
2020-04-03 22:26:14 -0400

    Updates

--- Projects/src/Array2.java
+++ Projects/src/Array2.java
@@ -124,6 +124,41 @@ public class Array2 {
 		  }
 		  return result;
 		}
+	
+	
+	
+	public boolean no14(int[] nums) {
+		boolean noOne = true, noFour = true;
+		for(int i = 0; i < nums.length && (noOne || noFour); i++) {
+			if(nums[i] == 1) {
+				noOne = false;
+			}else if(nums[i] == 4) {
+				noFour = false;
+			}
+		}	
+		return (noOne || noFour);
+	}
+
+	public boolean isEverywhere(int[] nums, int val) {
+		for(int i = 0; i < nums.length-1; i++) {
+			if(nums[i] != val && nums[i+1] != val) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean either24(int[] nums) {
+		int no2pair = 1, no4pair = 1;
+		for(int i = 0; i < nums.length - 1 && (no2pair + no4pair != 0); i++) {
+			if(nums[i] == 2 && nums[i+1] == 2) {
+				no2pair = 0;
+			}else if(nums[i] == 4 && nums[i+1] == 4) {
+				no4pair = 0;
+			}
+		}
+		return ((no2pair ^ no4pair) == 1);
+	}
 
 
 	public static void main(String[] args) {
