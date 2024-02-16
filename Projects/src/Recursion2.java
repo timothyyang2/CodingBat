3be3e786b7bf9cff19df9ecd5ca5c8c4e193ed12
timothyyang1 <tyang287@wisc.edu>
2024-02-16 12:04:23 -0600

    some codingbat recursion 2 exercises

--- Projects/src/Recursion2.java
+++ Projects/src/Recursion2.java
@@ -4,10 +4,7 @@ public class Recursion2 {
         if (start >= nums.length) {
             return target == 0;
         }
-        if (groupSum(start + 1, nums, target - nums[start])) {
-            return true;
-        }
-        if (groupSum(start + 1, nums, target)) {
+        if (groupSum(start + 1, nums, target - nums[start]) || groupSum(start + 1, nums, target)) {
             return true;
         }
         return false;
@@ -34,6 +31,38 @@ public class Recursion2 {
             return true;
         }
         if (groupNoAdj(start + 1, nums, target)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean groupSum5(int start, int[] nums, int target) {
+        if (start >= nums.length) {
+            return target == 0;
+        }
+        if (nums[start] % 5 == 0 && start + 1 < nums.length && nums[start + 1] == 1) {
+            return groupSum5(start + 2, nums, target - nums[start]);
+        }
+        if (nums[start] % 5 == 0) {
+            return groupSum5(start + 1, nums, target - nums[start]);
+        }
+        if (groupSum5(start + 1, nums, target - nums[start]) || groupSum5(start + 1, nums, target)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean groupSumClump(int start, int[] nums, int target) {
+        if (start >= nums.length) {
+            return target == 0;
+        }
+        int index = start;
+        int sum = 0;
+        while (index < nums.length && nums[start] == nums[index]) {
+            sum += nums[index];
+            index++;
+        }
+        if (groupSumClump(index, nums, target - sum) || groupSumClump(index, nums, target)) {
             return true;
         }
         return false;
