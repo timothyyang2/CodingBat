4442c8d0424e464d67189eeae9149f65abb40dfa
timothyyang1 <tyang287@wisc.edu>
2024-02-20 20:13:33 -0600

    some codingbat array 3 exercises

--- Projects/src/Array3.java
+++ Projects/src/Array3.java
@@ -58,6 +58,25 @@ public class Array3 {
         return false;
     }
 
+    public boolean linearIn(int[] outer, int[] inner) {
+        int i = 0;
+        int j = 0;
+        while (i < inner.length && j < outer.length) {
+            if (inner[i] > outer[j]) {
+                j++;
+            } else if (inner[i] < outer[j]) {
+                return false;
+            } else {
+                i++;
+            }
+        }
+
+        if (i != inner.length) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
     }
