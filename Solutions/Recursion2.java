21726e9494d7a5b49ab4f29261303806c0df7e82
timothyyang1 <tyang287@wisc.edu>
2024-03-16 09:13:22 -0500

    all codingbat recursion 2 exercises

--- Solutions/Recursion2.java
+++ Solutions/Recursion2.java
@@ -67,4 +67,55 @@ public class Recursion2 {
         }
         return false;
     }
+
+    public boolean splitArray(int[] nums) {
+        return splitArrayHelper(0, nums, 0, 0);
+    }
+
+    public boolean splitArrayHelper(int index, int[] nums, int sum1, int sum2) {
+        if (index >= nums.length) {
+            return sum1 == sum2;
+        }
+        if (splitArrayHelper(index + 1, nums, sum1 + nums[index], sum2)
+                || splitArrayHelper(index + 1, nums, sum1, sum2 + nums[index])) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean splitOdd10(int[] nums) {
+        return splitOdd10Helper(0, nums, 0, 0);
+    }
+
+    public boolean splitOdd10Helper(int index, int[] nums, int sum1, int sum2) {
+        if (index >= nums.length) {
+            return (sum1 % 10 == 0 && sum2 % 2 != 0) || (sum2 % 10 == 0 && sum1 % 2 != 0);
+        }
+        if (splitOdd10Helper(index + 1, nums, sum1 + nums[index], sum2)
+                || splitOdd10Helper(index + 1, nums, sum1, sum2 + nums[index])) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean split53(int[] nums) {
+        return split53Helper(0, nums, 0, 0);
+    }
+
+    public boolean split53Helper(int index, int[] nums, int sum1, int sum2) {
+        if (index >= nums.length) {
+            return sum1 == sum2;
+        }
+        if (nums[index] % 3 == 0) {
+            return split53Helper(index + 1, nums, sum1 + nums[index], sum2);
+        }
+        if (nums[index] % 5 == 0) {
+            return split53Helper(index + 1, nums, sum1, sum2 + nums[index]);
+        }
+        if (split53Helper(index + 1, nums, sum1 + nums[index], sum2)
+                || split53Helper(index + 1, nums, sum1, sum2 + nums[index])) {
+            return true;
+        }
+        return false;
+    }
 }
