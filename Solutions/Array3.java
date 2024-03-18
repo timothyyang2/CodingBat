a69b4aa0de94ae5d6af4d36e589c97c4158c7e32
timothyyang1 <tyang287@wisc.edu>
2024-03-18 15:56:43 -0500

    all codingbat array 3 exercises

--- Solutions/Array3.java
+++ Solutions/Array3.java
@@ -42,6 +42,27 @@ public class Array3 {
         return nums;
     }
 
+    public int[] fix45(int[] nums) {
+        int i = 0;
+        int j = 0;
+        while (j < nums.length && nums[j] != 5) {
+            j++;
+        }
+        while (i < nums.length) {
+            if (nums[i] == 4) {
+                int temp = nums[i + 1];
+                nums[i + 1] = nums[j];
+                nums[j] = temp;
+
+                while (j < nums.length && nums[j] != 5 || j == i + 1) {
+                    j++;
+                }
+            }
+            i++;
+        }
+        return nums;
+    }
+
     public boolean canBalance(int[] nums) {
         int first = 0;
         int second = 0;
@@ -75,5 +96,68 @@ public class Array3 {
             return false;
         }
         return true;
+    }
+
+    public int[] squareUp(int n) {
+        int[] result = new int[n * n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < n - i - 1) {
+                    continue;
+                }
+                result[i * n + j] = n - j;
+            }
+        }
+        return result;
+    }
+
+    public int[] seriesUp(int n) {
+        int current = 0;
+        int[] result = new int[n * (n + 1) / 2];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                result[current + j] = j + 1;
+            }
+            current += i;
+        }
+        return result;
+    }
+
+    public int maxMirror(int[] nums) {
+        int length = nums.length;
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+            count = 0;
+            for (int j = length - 1; j >= 0 && i + count < length; j--) {
+                if (nums[i + count] == nums[j]) {
+                    count++;
+                } else if (count > 0) {
+                    max = Math.max(count, max);
+                    count = 0;
+                }
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+
+    public int countClumps(int[] nums) {
+        int count = 0;
+        int length = 0;
+        int index = 0;
+        while (index < nums.length) {
+            int current = nums[index];
+            index++;
+            while (index < nums.length && nums[index] == current) {
+                length++;
+                index++;
+            }
+            if (length > 0) {
+                count++;
+            }
+            length = 0;
+        }
+        return count;
     }
 }
